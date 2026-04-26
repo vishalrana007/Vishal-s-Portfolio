d54692e26e26a97ebde6d1e89b1e0622b83593bf
vishalran007 <vr772107@gmail.com>
2026-04-26 21:43:12 +0530

    changes added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
-  distDir: ".next-app",
   turbopack: {
     root: path.join(__dirname),
   },
